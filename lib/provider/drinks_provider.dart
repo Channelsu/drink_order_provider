00cd6ccd953a88327948a8fe814f5d7e3dd8fbf0
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-03-25 23:56:15 +0900

    ドリンクプロバイダークラスの作成作成

--- lib/provider/drinks_provider.dart
+++ lib/provider/drinks_provider.dart
@@ -1,6 +1,21 @@
+import 'package:flutter/foundation.dart';
+import 'package:drinkorderprovider/models/drink.dart';
+
 // TODO 1: ドリンクのプロバイダークラス作成
 // TODO 2: ChangeNotifierクラスを継承
 // TODO 3: drinks配列の状態を初期化
 // TODO 4: drinksのゲッター作成
 // TODO 10: Implement the selectDrink method
 // TODO 12: Implement a getter function for selected Drinks
+
+class DrinkProvider extends ChangeNotifier {
+  // 外から変更されてはいけないのでプライベート化
+  List<Drink> _drinks = [
+    Drink("水", false),
+    Drink("コーラ", false),
+    Drink("ポカリ", false),
+    Drink("十六茶", false)
+  ];
+
+  List<Drink> get drinks => _drinks;
+}
