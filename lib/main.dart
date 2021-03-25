b9c4a43128dfd61d9bdc65edc7c4a19b25e01277
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-03-26 08:07:20 +0900

    ChangeNotifierProviderを設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:drinkorderprovider/provider/drinks_provider.dart';
 import 'package:drinkorderprovider/setState/checkbox_setState_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,9 +15,12 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'ドリンク',
       theme: ThemeData.light().copyWith(primaryColor: Color(0xFFe13133)),
-      // TODO 5: ChangeNotifierProviderを登録
-      // TODO 6: createメソッドを追加。DrinksProviderのインスタンスを生成
-      home: CheckBoxSetStateScreen(),
+      // TODO 5: ChangeNotifierProviderで覆う
+      // TODO 6: createプロパティを追加。DrinksProviderのインスタンスを生成
+      home: ChangeNotifierProvider(
+        create: (_) => DrinkProvider(),
+        child: CheckBoxSetStateScreen()
+      ),
     );
   }
 }
